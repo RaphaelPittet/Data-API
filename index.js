1178656e495a1a24ef6fcb4b4d414a09b572830e
RaphaelPittet <pittetr.96@gmail.com>
2022-03-06 20:06:55 +0100

    2/3 endpoint - not tested by automated test

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 3000;
+
+// Where we will keep chat history 
+let chatHistory = [];
+
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post('/data/:customerId/:dialogId', (req, res) => {
+    let customerId = req.params.customerId;
+    let dialogId = req.params.dialogId;
+    let data = req.body;
+    let date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    let chat = {
+        dialogId: dialogId,
+        customerId: customerId,
+        text: data.text,
+        language: data.language,
+        timestamp: date
+    }
+
+    chatHistory.push(chat)
+    console.log(chatHistory)
+    res.sendStatus(200)
+});
+
+app.post('/consents/:dialogId', (req,res) => {
+    let message = "";
+    let dialogId = req.params.dialogId;
+    // we imagine payload is {consents: true/false}
+    let toSave = req.body.consents;
+    
+    if (toSave){
+        // save data to Database
+        message = `the chat with dialogId ${dialogId} will be saved in the DB`;
+    }else{
+        // remove all history of the dialog with dialogId
+        message = "history will be remove"
+        for (i=0; chatHistory.length; i++){
+            if(chatHistory[i].dialogId == dialogId){
+                chatHistory.splice(i,1);
+                i--;
+            }
+        }
+    }
+    console.log(chatHistory);
+    res.status(200).json({response: message});
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
+
+
+
