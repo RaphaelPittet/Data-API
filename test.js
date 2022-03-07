6a7ab6c2adfb04460c2827bfe8a46054c9e1a905
RaphaelPittet <pittetr.96@gmail.com>
2022-03-07 18:53:42 +0100

    Data-API - test Script - 2/3 endpoint test

--- test.js
+++ test.js
@@ -3,15 +3,26 @@ const axios = require('axios');
 const dialogue1 = ["hello", "i want to contact the support", "by email", "thanks"];
 const dialogue2 = ["hello", "i want to konw the price of an item", "item name", "the bigger one"];
 
-makePostDialogue(dialogue1, "12345", "54321");
-makePostDialogue(dialogue2, "12345", "34567");
-//consentsQuestion("34567");
+let dialog2 = makePostDialogue(dialogue2[0], "54321", "34567");
+let dialog3 = makePostDialogue(dialogue2[1], "54321", "34567");
+let dialog4 = makePostDialogue(dialogue2[2], "54321", "34567");
+let dialog5 = makePostDialogue(dialogue2[3], "54321", "34567");
+let dialog1 = makePostDialogue(dialogue1[0], "12345", "54321");
+let dialog6 = makePostDialogue(dialogue1[1], "12345", "54321");
+let dialog7 = makePostDialogue(dialogue1[2], "12345", "54321");
+let dialog8 = makePostDialogue(dialogue1[3], "12345", "54321");
 
-function makePostDialogue( dialog, dialogId, customerId) {
 
-    for(i=0; i<dialog.length; i++){
-        let payload = {text: dialog[1], language: "EN"}
-    axios.post('http://localhost:3000/data/'+ customerId + '/' + dialogId, payload)
+
+Promise.all([dialog1, dialog2,dialog3, dialog4, dialog5,dialog6,dialog7,dialog8]).then((values) =>{
+    consentsQuestion("54321", true);
+    consentsQuestion("12345", false) 
+}) 
+
+async function makePostDialogue( dialog, dialogId, customerId) {
+
+    let payload = {text: dialog, language: "EN"}
+    await axios.post('http://localhost:3000/data/'+ customerId + '/' + dialogId, payload)
     .then(res => {
         console.log(res.status);
         
@@ -20,12 +31,10 @@ function makePostDialogue( dialog, dialogId, customerId) {
         console.log(error);
     });
 }
-   
-}
- /*
-async function consentsQuestion (dialogId){
+ 
+async function consentsQuestion (dialogId, choice){
 
-    await axios.post('http://localhost:3000/consents/'+ dialogId)
+    await axios.post('http://localhost:3000/consents/'+ dialogId, {consents: choice})
     .then(res => {
         console.log(res.status);
         
@@ -34,4 +43,3 @@ async function consentsQuestion (dialogId){
         console.log(error);
     });
 }
-*/
