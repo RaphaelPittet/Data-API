3f158c249d43b9b132a99a1733d2d5d17ef35048
RaphaelPittet <pittetr.96@gmail.com>
2022-03-07 18:52:50 +0100

    Data-API update: 2/3 Endpoint

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.post('/data/:customerId/:dialogId', (req, res) => {
 app.post('/consents/:dialogId', (req,res) => {
     let message = "";
     let dialogId = req.params.dialogId;
+
     // we imagine payload is {consents: true/false}
     let toSave = req.body.consents;
     
@@ -45,23 +46,23 @@ app.post('/consents/:dialogId', (req,res) => {
         // save data to Database
         message = `the chat with dialogId ${dialogId} will be saved in the DB`;
     }else{
+
         // remove all history of the dialog with dialogId
         message = "history will be remove"
         for (i=0; i<chatHistory.length; i++){
-            console.log("saved dialogId: " +chatHistory[i].dialogId);
-            console.log("params dialogId: " +dialogId);
-            console.log("--------------------------------");
-
 
             if(chatHistory[i].dialogId == dialogId){
-                console.log(chatHistory[i]);
                 chatHistory.splice(i,1);
                 i--;
             }
         }
     }
+    console.log("______________________________________________________--")
+    console.log(chatHistory);
     res.status(200).json({response: message});
-})
+});
+
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
 
